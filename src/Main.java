36277ebcaae15a23638eb4cfabda8a5f0a1b7d9a
AlexT6665 <taingalexandre75@outlook.fr>
2025-03-26 19:41:49 +0100

    Part 1 - Make Request

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.sql.*;
 
 
@@ -17,6 +18,9 @@ public class Main {
 
         try {
             connection = DriverManager.getConnection( url, user, password );
+            // displayDepartment( connection );
+            // moveDepartment(connection, 7499, 30);
+            //displayTable(connection, "emp");
 
             System.out.println("Db is connected");
         } catch (SQLException e) {
@@ -30,7 +34,62 @@ public class Main {
                 }
             }
         }
+    }
 
-        System.out.println("Hello, World!");
+    public static void displayDepartment(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery( "SELECT * FROM dept" );
+
+        while (resultSet.next()) {
+            int deptno = resultSet.getInt( "deptno" );
+            String dname = resultSet.getString( "dname" );
+            String location = resultSet.getString("loc");
+
+            System.out.println("Department " + deptno + " is for "
+                    + dname + "located in " + location);
+        }
+        resultSet.close();
+    }
+
+    public static void moveDepartment(Connection connection, int empno, int deptno) throws SQLException {
+        String query = "UPDATE emp SET deptno = " + deptno + " WHERE empno = " + empno;
+
+        try (Statement statement = connection.createStatement()) {
+            int rowsAffected = statement.executeUpdate( query );
+
+            // Check if the employee was moved
+            if (rowsAffected > 0) {
+                System.out.println("Employee " + empno + " moved to department " + deptno);
+            } else {
+                System.out.println("No employee found with empno " + empno);
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to move employee: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    public static void displayTable(Connection connection, String tableName) throws SQLException {
+        String query = "SELECT * FROM " + tableName;
+
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(query)) {
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
+            }
+            System.out.println();
+
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to display table: " + e.getMessage());
+            throw e;
+        }
     }
 }
